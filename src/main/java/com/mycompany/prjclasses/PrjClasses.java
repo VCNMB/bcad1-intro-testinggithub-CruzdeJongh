d0006d7fb50f3217360a4ec1ea9a2b068daf8a0f
CruzdeJongh <ST10473274@vcconnect.edu.za>
2025-03-11 10:55:21 +0200

    fix

---
+++ src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjclasses;
+
+/**
+ *
+ * @author cruzdejongh
+ */
+public class PrjClasses {
+
+    public static void main(String[] args) {
+        
+    newClass nc = new newClass(); //creating an object of newClass
+    
+    nc.helloClass(); // have tp use variable name nc to communicate with the class
+    
+        System.out.println(nc.strSecondClassVar); //output variable from 2nd class
+    
+    
+    }
+    
+}
